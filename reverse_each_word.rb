9dae0c5940b97e89b7a9834b6865bd4d374f230e
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-29 01:56:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(sentence)
-  array = sentence.chars
-  array.reverse
-  
+  array = sentence.split.each_slice(1).map{|a|a.join ' '}
+  reverse_array = []
+  array.each do |word|
+    reverse_array << word.reverse
+  end
+  reverse_array
 end 
